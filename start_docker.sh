b1a60e52d73c1531fa8d694572b226aad426fd80
nrv-96 <62826272+nrv-96@users.noreply.github.com>
2024-02-09 16:51:17 +0530

    Update start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -7,7 +7,7 @@ docker run --name jenkins-blueocean --restart=on-failure --detach \
   --publish 8080:8080 --publish 50000:50000 \
   --volume jenkins-data:/var/jenkins_home \
   --volume jenkins-docker-certs:/certs/client:ro \
-  docker pull nirav96/jenkins:latest
+   nirav96/jenkins:latest
 id=$(docker ps --format "{{.ID}}")
 jenkins_password=$(docker exec $id cat  /var/jenkins_home/secrets/initialAdminPassword)
 echo "Jenkins Admin Password: "$jenkins_password
