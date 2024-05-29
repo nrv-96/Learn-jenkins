e4a7b19e2e89ff963344393ebb9972704fe882da
nrv-96 <62826272+nrv-96@users.noreply.github.com>
2024-05-29 10:04:54 +0530

    Update start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #docker build -t myjenkins-blueocean:2.414.2 .
+docker_container_name="jenkins-blueocean"
 docker network create jenkins
-docker run --name jenkins-blueocean --restart=on-failure --detach \
+docker run --name "$docker_container_name" --restart=on-failure --detach \
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
   --publish 8080:8080 --publish 50000:50000 \
@@ -9,8 +10,8 @@ docker run --name jenkins-blueocean --restart=on-failure --detach \
   --volume jenkins-docker-certs:/certs/client:ro \
    nirav96/jenkins:latest
 sleep 5
-id=$(docker ps --format "{{.ID}}")
+container_id=$(docker ps -qf "name=$docker_container_name")
 sleep 5
-jenkins_password=$(docker exec $id cat  /var/jenkins_home/secrets/initialAdminPassword)
+jenkins_password=$(docker exec "$container_id" cat  /var/jenkins_home/secrets/initialAdminPassword)
 echo "Jenkins Admin Password: "$jenkins_password
 # http://localhost:8080/
